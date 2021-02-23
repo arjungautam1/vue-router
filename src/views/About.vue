d9605eefe8d33334b20e855848a1fd265cdbd700
arjun-sudo <arjungautam5431@gmail.com>
2021-02-23 14:48:37 +0545

    React Router

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>I am Arjun Gautam from FocusOne payment Solution . I am currently learning vue.js and spark java parallely. </h1>
+  </div>
+</template>
---
+++ src/views/Contact.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="contact">
+    <h1>
+      Contact Details
+      <p>email : arjugautam5431@gmail.com</p>
+      <p>Phone Number :9863482273</p>
+    </h1>
+  </div>
+</template>
